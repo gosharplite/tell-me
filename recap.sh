7f78a2f617e2974c69c3e56a069f678bc05d2808
Tony Hsu <gosharplite@gmail.com>
2026-01-16 14:30:14 +0800

    re

--- recap.sh
+++ recap.sh
@@ -11,6 +11,7 @@ RAW_MODE="false"
 CODE_MODE="false"
 LAST_MESSAGES=0
 LAST_PAIRS=0
+SUMMARY_MESSAGES=0 # NEW: Variable for summary mode
 
 # Check environment variable override
 if [ "${RAW:-false}" = "true" ]; then RAW_MODE="true"; fi
@@ -45,6 +46,15 @@ while [[ $# -gt 0 ]]; do
             LAST_MESSAGES=1 # Force showing last message for code extraction
             shift
             ;;
+        # NEW: Argument for summary view
+        -s|--summary)
+            SUMMARY_MESSAGES=10 # Default to last 10 if no number is given
+            if [[ "$2" =~ ^[0-9]+$ ]]; then
+                SUMMARY_MESSAGES=$2
+                shift
+            fi
+            shift
+            ;;
         *)
             FILERECAP="$1"
             shift
@@ -64,7 +74,9 @@ if ! command -v jq &> /dev/null; then
 fi
 
 # --- Determine JQ Filter Scope ---
-if [ "$LAST_PAIRS" -gt 0 ]; then
+if [ "$SUMMARY_MESSAGES" -gt 0 ]; then # NEW: Prioritize summary scope
+    JQ_PREFIX=".messages[-${SUMMARY_MESSAGES}:] | .[]"
+elif [ "$LAST_PAIRS" -gt 0 ]; then
     NUM_TO_SLICE=$(( LAST_PAIRS * 2 ))
     JQ_PREFIX=".messages[-${NUM_TO_SLICE}:] | .[]"
 elif [ "$LAST_MESSAGES" -gt 0 ]; then
@@ -75,6 +87,22 @@ else
 fi
 
 # --- Rendering ---
+
+# NEW: Mode 0: Summary View
+if [ "$SUMMARY_MESSAGES" -gt 0 ]; then
+    jq -r "
+      $JQ_PREFIX |
+      (
+        # 1. Set the role prefix with color
+        (if .role == \"user\" then \"\\u001b[1;32m[USER] \" else \"\\u001b[1;34m[MODEL]\" end) + \"\\u001b[0m: \" +
+        # 2. Get all text content, replace newlines with spaces, and collapse multiple spaces
+        ((.parts // []) | map(.text // \"\") | join(\"\") | gsub(\"\\n\"; \" \") | gsub(\"[ \t]+\"; \" \")) |
+        # 3. Truncate the resulting one-liner if it's longer than 120 characters
+        (if length > 120 then .[0:117] + \"...\" else . end)
+      )
+    " "$FILERECAP"
+    exit 0
+fi
 
 # Mode 1: Code/Content Only
 if [ "$CODE_MODE" = "true" ]; then
